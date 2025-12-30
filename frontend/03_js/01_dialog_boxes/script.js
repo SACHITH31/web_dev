1e6d21fdd15cc3bc3022e6b4b0b6286fc029e9de
SACHITH <kasasachith@gmail.com>
2025-12-30 19:28:10 +0530

    completed about the dialog boxes

---
+++ frontend/03_js/01_dialog_boxes/script.js
@@ -0,0 +1,13 @@
+//typeof alert is undefined 
+// alert("Hello! Welcome to JavaScript");
+
+
+//confirm returns true if we click on ok 
+//confirm returns false if we click on cancel 
+//typeof confirm is boolean
+// const a = confirm("Do you want to continue?");
+// console.log(a)
+
+
+// const result = prompt("Enter your name:");
+// console.log(typeof(result)); //returns string
