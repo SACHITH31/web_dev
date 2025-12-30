44be05c289fa3c27a43fb15e57939d7236c0b25a
SACHITH <kasasachith@gmail.com>
2025-12-30 20:17:38 +0530

    compelted the strings part

--- frontend/03_js/01_dialog_boxes/script.js
+++ frontend/03_js/01_dialog_boxes/script.js
@@ -1,7 +1,9 @@
+//    ALERT
 //typeof alert is undefined 
 // alert("Hello! Welcome to JavaScript");
 
 
+//    CONFIRM
 //confirm returns true if we click on ok 
 //confirm returns false if we click on cancel 
 //typeof confirm is boolean
@@ -9,5 +11,16 @@
 // console.log(a)
 
 
+//    PROMPT
 // const result = prompt("Enter your name:");
 // console.log(typeof(result)); //returns string
+
+
+// ⚠ Important Points: 
+// i: Dialog boxes stop the page until user clicks a button
+// ii: Mostly used for learning or simple messages
+// iii: Not used much in modern websites (we use custom popups)
+
+// alert() → show message
+// confirm() → yes or no
+// prompt() → take input
---
+++ frontend/03_js/02_strings/script.js
@@ -0,0 +1,29 @@
+//Strings
+let userName = "Sachith";
+
+// string literals(``)
+let age = 20;
+let msg = `My age is ${age}`;
+
+let secondChar = userName[2]
+
+
+
+// ❓ Properties vs Methods
+
+// 1️⃣ String Properties
+// 👉 Property = gives information
+// 👉 No () brackets
+// Example:
+// let str = "Hello";
+// str.length
+// ✔ length is a property
+
+// 2️⃣ String Methods
+// 👉 Method = does some work
+// 👉 Always has ()
+// Example:
+// str.toUpperCase()
+// str.toLowerCase()
+// ✔ These change / process the string
+// ✔ They are actions
