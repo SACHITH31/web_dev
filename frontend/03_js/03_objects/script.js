8e16d85e2ff69e4cf6511b38bf8c0396090342a4
SACHITH <kasasachith@gmail.com>
2025-12-31 09:42:33 +0530

    completed about the objects

--- frontend/03_js/03_objects/script.js
+++ frontend/03_js/03_objects/script.js
@@ -21,6 +21,10 @@ let keys = Object.keys(myObj)
 let values = Object.values(myObj)
 
 
+// usage of 'in'
+console.log('hello' in myObj) // returns false because hello(key) is not there in myObj 
+console.log('firstName' in myObj) // returns true because firstName(key) is in the myObj
+
 // to change the key in an object
 let user = {
     firstName: 'John',
@@ -30,7 +34,7 @@ let user = {
 // 1. Read and assign to the new key
 user.userAge = user.age;
 // 2. Delete the old key
-delete user.age;
+delete user.age; // deelte returns true or false only 
 console.log(user);
 // Output: { firstName: 'John', lastName: 'Doe', userAge: 30 }
 
@@ -52,6 +56,7 @@ console.log(d[0]); // ['22', 'rollNumber']
 console.log(d[0][0]); // 22
 
 
+
 // Objects store references to nested objects, not copies. The parent object holds memory addresses (pointers) to child objects.
 //          EXAMPLE: 
 // Parent: { child1: [address1], child2: [address2] }
@@ -65,4 +70,4 @@ console.log(d[0][0]); // 22
 // whenever we add some data to the exsting object then it address won't change instead the particular new value will gets added only into this object
 // whenever we add some data into an mulitple leveled object then that particular multi leveled objects address won't gets changes instead the new value gets added only no address gets changed
 // when ever we modify the existing value of an key then it's address will gets changed only(REMEMBER it's parent(this particular key-value object) address won't gets changed)
-
+// whenever we modify the name of the object names then that particular object address will gets changes
--- frontend/03_js/04_object_freeze_object_seal/script.js
+++ frontend/03_js/04_object_freeze_object_seal/script.js
@@ -9,6 +9,17 @@ const myObj = {
 }
 
 // IMPORTANT NOTES: 
-// whenever we add some data to the exsting object then it address won't change instead the particular new value will gets added only into this object
-// whenever we add some data into an mulitple leveled object then that particular multi leveled objects address won't gets changes instead the new value gets added only no address gets changed
-// when ever we modify the existing value of an key then it's address will gets changed only(REMEMBER it's parent(this particular key-value object) address won't gets changed)
+// Object.seal(objectname-here); 
+// ------ this is used to seal the object which means:
+// ------- i) no one can delete the exisiting data or 
+// ------- ii) no new values gets addeded 
+// ------- iii) modification on the exisiting data can be happened   
+// conclusion : modify or change(YES), add(NO), delete(NO)
+
+
+// Object.freeze(objectname-here); 
+// ------ this is used to seal the object which means:
+// ------- i) no one can delete the exisiting data or 
+// ------- ii) no new values gets addeded 
+// ------- iii) no modifications will be done on existing data 
+// conclusion : modify or change(NO), add(NO), delete(NO)
