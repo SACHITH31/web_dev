1909cd9099a62e44ddb5dbf8842bf140c90a38f4
SACHITH <kasasachith@gmail.com>
2025-12-31 09:16:45 +0530

    added about the changing and deletion of keys

--- frontend/03_js/03_objects/script.js
+++ frontend/03_js/03_objects/script.js
@@ -59,3 +59,10 @@ console.log(d[0][0]); // 22
 // Parent unchanged
 
 // Modifying a nested object changes the original because both parent and code point to the same memory location.
+
+
+// IMPORTANT NOTES: 
+// whenever we add some data to the exsting object then it address won't change instead the particular new value will gets added only into this object
+// whenever we add some data into an mulitple leveled object then that particular multi leveled objects address won't gets changes instead the new value gets added only no address gets changed
+// when ever we modify the existing value of an key then it's address will gets changed only(REMEMBER it's parent(this particular key-value object) address won't gets changed)
+
