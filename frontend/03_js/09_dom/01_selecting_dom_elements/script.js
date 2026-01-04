f84a50e8c519e052a441721f1f78e506cfcb3097
SACHITH <kasasachith@gmail.com>
2026-01-04 21:55:30 +0530

    displays the username

--- frontend/03_js/09_dom/01_selecting_dom_elements/script.js
+++ frontend/03_js/09_dom/01_selecting_dom_elements/script.js
@@ -2,6 +2,15 @@ const userName = document.querySelector('.userName')
 const password = document.querySelector('.password')
 const submitButton = document.querySelector('.submitButton')
 
+const showResults = document.querySelector('.showResults')
+
 submitButton.addEventListener('click', () => {
-    console.log('Clicked')
+    showResults.innerHTML = '';
+    const userNameTag = document.createElement('p')
+    const passwordTag = document.createElement('p')
+
+    userNameTag.innerText = userName.value
+    passwordTag.innerText = password.value
+
+    showResults.append(userNameTag)
 })
