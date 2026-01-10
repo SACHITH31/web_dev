785432d211aba16d6f53aa9995e9f9db269f2035
SACHITH <kasasachith@gmail.com>
2026-01-10 06:54:12 +0530

    learnt event loupe

--- frontend/03_js/10_ajax/01_eventloupe/script.js
+++ frontend/03_js/10_ajax/01_eventloupe/script.js
@@ -25,10 +25,32 @@
 // console.log("End");
 
 
-function myFunc () {
-    console.log("Hello From myfunc")
-}
-setTimeout(myFunc, 2000);
+// function myFunc () {
+//     console.log("Hello From myfunc")
+// }
+// setTimeout(myFunc, 2000);
 // IMPORTANT POINT: 
 // whenever we call a function in a setTimeOut then we should not call the function with () instead we have to write the name of that function only.
 // if we write the function call thenn it won't act as the async, instead it acts as the synchronous
+
+
+console.log('Hi-1');
+console.log('Hi-2');
+console.log('Hi-3');
+
+setTimeout(() => {
+    console.log('settime-1')
+}, 2000);
+
+setTimeout(() => {
+    console.log('settime-2')
+}, 1000);
+
+
+for (let i = 1; i <= 50; i++) {
+    console.log(i)
+}
+
+setTimeout(() => {
+    console.log('settime-3')
+}, 0);
