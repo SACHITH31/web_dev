1cd199cb4bd04e45a3d8a2715ac2fb77cf7e917b
SACHITH <kasasachith@gmail.com>
2026-01-12 07:18:45 +0530

    written in detail about this for in and for of

--- frontend/03_js/12_loops/01_for_of_loop/script.js
+++ frontend/03_js/12_loops/01_for_of_loop/script.js
@@ -9,10 +9,7 @@ const myObj = {
     'name': 'sachith',
     'age': 19,
     'clg': 'GPREC',
-    'address': {
-        'city': 'kurnool',
-        'state': 'andhra pradesh'
-    }
+    'address': 'kurnool'
 }
 console.log(myObj);
 // for (let temp of myObj) {
@@ -22,6 +19,25 @@ for (let temp in myObj) {
     console.log(temp, myObj[temp]);
 } // as i said 'in' prints the indexes and here the indexes are the keys so we see the keys
 
+const userDatas = Object.entries(myObj)
+
+for (const userData of userDatas) {
+    // console.log(userDatas[userData]); // if in
+    console.log(userData); // if of
+}
+
+
+
+
+console.log('PRINTING INDEXES USING IN LOOP')
+for (const data in 'sachith') {
+    console.log(data)
+}
+
+console.log('PRINTING VALUES USING OF LOOP')
+for (const data of 'sachith') {
+    console.log(data)
+}
 
 // THE MAIN DIFFERENCE BETWEEN THE FOR LOOP AND THIS IS: 
 // here automatically a new variable gets created in this for of and for in loop.
