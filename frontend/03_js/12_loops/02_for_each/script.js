d02f71e44d49a44df2fbb9fc039f8aa0e2aeca25
SACHITH <kasasachith@gmail.com>
2026-01-17 21:28:53 +0530

    completed about the for each

--- frontend/03_js/12_loops/02_for_each/script.js
+++ frontend/03_js/12_loops/02_for_each/script.js
@@ -14,16 +14,22 @@ const fruits = ['banana', 'apple', 'mango', 'watermelon']
 // })
 
 
-fruits.forEach(function (fruit, i) {
-    console.log(fruit, i)
-})
+// fruits.forEach(function (fruit, i) {
+//     console.log(fruit, i)
+// })
 
 
-fruits.forEach(function (fruit, i, fruitsList) {
-    console.log(fruit, i, fruitsList)
-})
+// fruits.forEach(function (fruit, i, fruitsList) {
+//     console.log(fruit, i, fruitsList)
+// })
 
 // Note: we can pass 3 parameters 
 // 1st. to access each element
 // 2nd. to get the position
 // 3rd. will have the total list
+
+
+fruits.forEach(function (fruit) {
+    console.log(fruit)
+    return fruit.toUpperCase() // here we won't see the fruit as the upper case and when we try to run this code(line no: 32-35) then we will see undefined even if we keep the return
+})
---
+++ frontend/03_js/13_spread_operator/script.js
@@ -0,0 +1,2 @@
+const nums1 = [1, 2, 3, 4]
+const nums2 = [5, 6, 7, 8, 9]
