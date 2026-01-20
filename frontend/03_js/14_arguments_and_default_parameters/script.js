3a7eae3242c385499b04f790ce9bef831a0b7c4c
SACHITH <kasasachith@gmail.com>
2026-01-20 22:09:52 +0530

    written about some sample code

--- frontend/03_js/14_arguments_and_default_parameters/script.js
+++ frontend/03_js/14_arguments_and_default_parameters/script.js
@@ -2,13 +2,26 @@
 // it looks like array like but it is not an array. How can we say? Ans: because we can't apply the array methods like map, reduce, fiter, forEach
 // we can apply the length method and also we can implement the for loop
 
-function add (a, b) {
+function add1 (a, b) {
     console.log(arguments)
     console.log(arguments[0])
     console.log(arguments[1])
     console.log(arguments.length)
 }
 
-// add(1, 2, 3)
-// add(1, 2, 3, 4, 5, 6)
-// add(1, 2, 3, 4, 5, 6, 7, 8)
+// add1(1, 2, 3)
+// add1(1, 2, 3, 4, 5, 6)
+// add1(1, 2, 3, 4, 5, 6, 7, 8)
+
+
+// TYPES TO WRITE THE FUNCTIONS 
+const add2 = function () {
+    console.log(arguments)
+}
+
+// ARROW FUNCTIONS 
+const add3 = () => { // WHEN WE CALL THIS THEN WE WILL GET THE ERROR AS THE ARGUMENTS IS NOT DEFINED BECAUSE THIS ARROW FUNCTION IS NEW ONE AND WHERE AS THIS ARGUMENTS IS OLDER ONE
+                     // THE ALTERNATIVE OF THIS ARGUMENTS IS REST PARAMETER.
+    console.log(arguments)
+}
+
