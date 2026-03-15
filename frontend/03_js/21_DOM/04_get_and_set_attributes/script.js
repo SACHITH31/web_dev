36bd9c411b913ccde8e3d47b7cd133b366edc135
SACHITH <kasasachith@gmail.com>
2026-03-15 19:47:37 +0530

    written about the attributes

---
+++ frontend/03_js/21_DOM/04_get_and_set_attributes/script.js
@@ -0,0 +1,21 @@
+// what are attribute:🤔🤔 ??
+// In HTML, attributes are extra information added to an HTML element.
+// They modify or control the behavior of an element.
+
+// Think of it like this:
+
+// 👉 HTML tag = the object
+// 👉 Attribute = extra details about that object
+
+{/* <tag attribute="value">Content</tag> */}
+// attribute → the name of the property
+// value → (the setting or data for that property) or (the value of the attribute)
+
+// EXAMPLE : <a href = 'https:google.com'>Go TO Google </a>
+// here a is tag name
+// here href is the ATTRIBUTE
+// here https:google.com is the value of the attribute
+
+
+// common attributes: id, class, src, href, alt, title, style, type, placeholder, required, etc.....,,,,
+
