24ee548f5786dd7842afccf8e76a7ef63fd019bb
SACHITH <kasasachith@gmail.com>
2026-05-14 18:54:23 +0530

    covered main math methods

---
+++ frontend/03_js/03_math/math.js
@@ -0,0 +1,18 @@
+// CEIL AND FLOOR
+
+// ceil means top so it returns the top most value.
+// ex: console.log(Math.ceil(4.1)); // 5
+// ex: console.log(Math.ceil(4.9)); // 5
+
+
+// floor means bottom or below so it returns the bottom most value.
+// ex: console.log(Math.ceil(4.1)); // 4
+// ex: console.log(Math.ceil(4.9)); // 4
+
+
+// exp- it returns e power x, where e value is  2.718
+// ex: Math.exp(1) // 2.718281828459045
+
+
+// trunc - it is used to return the integer part only
+// ex: Math.trunc(12.22) // 12
