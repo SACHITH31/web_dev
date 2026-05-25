f6e82cc6a1bfb9182880c3a579f14ba2a929c425
SACHITH <kasasachith@gmail.com>
2026-05-25 13:09:45 +0530

    written about the comparision operator

---
+++ frontend/03_js/00_operators/comparision/script.js
@@ -0,0 +1,7 @@
+// == (double equals to)
+// this won't checks whether the 2 variables are of having same data types instead they check if the both the values are same then it does implicit conversion
+
+
+// === (triple equals to)
+// this first checks whether those 2 variables are of same data type. if yes then they check values, else it returns false
+// IT DOES STRICTLY EQUALITY
