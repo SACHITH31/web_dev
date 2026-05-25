4891611d962beccdd997e03d2f5f8a78448b7605
SACHITH <kasasachith@gmail.com>
2026-05-25 13:13:31 +0530

    updated the comparision notes

--- frontend/03_js/00_operators/comparision/script.js
+++ frontend/03_js/00_operators/comparision/script.js
@@ -1,7 +1,8 @@
 // == (double equals to)
-// this won't checks whether the 2 variables are of having same data types instead they check if the both the values are same then it does implicit conversion
+// this won't checks whether the 2 variables are of having same data types instead they check if the both the values are same then it does implicit conversion(automatic conversion)
 
 
 // === (triple equals to)
 // this first checks whether those 2 variables are of same data type. if yes then they check values, else it returns false
 // IT DOES STRICTLY EQUALITY
+// HERE TO GET TRUE, WE HAVE TO DO EXPLICIT CONVERSION(MANUAL CONVERSION)
